8890b38945ece562b0d393a2196ff152d808906f
joakom24 <joakoomussi@gmail.com>
2022-05-24 20:07:53 -0300

    added icons

--- components/Poker.js
+++ components/Poker.js
@@ -1,5 +1,8 @@
 import Image from "next/image";
 import table from "../assets/tablePoker.png";
+import less from "../assets/less.png";
+import more from "../assets/more.png";
+import bars from "../assets/bars.png";
 const Poker = () => {
   return (
     //PokerInterface
@@ -25,18 +28,23 @@ const Poker = () => {
       </div>
       {/*Buttons right*/}
       <div className="absolute bottom-6 text-white right-4 flex ">
+        {/*Buttom number*/}
         <button className="mr-2  bg-primary px-4 py-4 rounded-sm  border-t-1 border-buttom border-b-1 drop-shadow-5xl">
           20
         </button>
-        <div className="bg-primary flex justify-between w-96 rounded-large border-buttom border-t-1 border-b-1">
-          <button className="m-2  font-bold bg-pinkPoker rounded-full px-4 py-2 border-2 border-pinkPokerBorder shadow-4xl">
-            -
+
+        <div className="bg-primary flex justify-between w-96 rounded-large max-h-12 border-buttom border-t-1 border-b-1">
+          {/*Button less*/}
+          <button className="m-3  flex items-center  bg-pinkPoker rounded-full px-3 py-1 border-2 border-pinkPokerBorder shadow-4xl">
+            <Image src={less} alt="lessIcon" />
           </button>
-          <button className="w-64 border-1 rounded-full m-auto h-3   bg-pinkPoker">
-            []
-          </button>
-          <button className="m-2  font-bold bg-pinkPoker rounded-full px-4 py-2 border-2 border-pinkPokerBorder shadow-4xl">
-            +
+          {/*Button move*/}
+          <div className=" flex justify-center w-64 border-1 rounded-full m-auto h-3   bg-pinkPoker">
+            <Image src={bars} alt="barsIcon" className="" />
+          </div>
+          {/*Buttons add*/}
+          <button className="m-3 flex items-center  bg-pinkPoker rounded-full px-3 py-1 border-2 border-pinkPokerBorder shadow-4xl">
+            <Image src={more} alt="moreIcon" />
           </button>
         </div>
       </div>
