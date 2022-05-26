f5db92664de6e83cfb108cff46378b5fb7e9b40d
joakom24 <joakoomussi@gmail.com>
2022-05-26 16:08:34 -0300

    changed buttons more and less

--- components/Poker.js
+++ components/Poker.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import table from "../assets/tablePoker.png";
-import less from "../assets/less.png";
-import more from "../assets/more.png";
-import bars from "../assets/bars.png";
+import less from "../assets/lessIcon.png";
+import more from "../assets/moreIcon.png";
+import bars from "../assets/barsIcon.png";
+import back from "../assets/backArrow.png";
 const Poker = () => {
   return (
     //PokerInterface
     <div className="bg-[url('../assets/backgroundPoker.png')] bg-no-repeat bg-cover left-0 top-0 h-screen flex justify-center items-center">
+      <div className="">
+        <Image src={back} alt="backIcon" />
+      </div>
       {/*ImagePokerTable*/}
       <div>
         <Image src={table} alt="tablePoker" />
@@ -33,17 +37,17 @@ const Poker = () => {
           20
         </button>
 
-        <div className="bg-primary flex justify-between w-96 rounded-large max-h-12 border-buttom border-t-1 border-b-1">
+        <div className="bg-primary flex justify-between items-center w-96 rounded-large max-h-12 border-buttom border-t-1 border-b-1">
           {/*Button less*/}
-          <button className="m-3  flex items-center  bg-pinkPoker rounded-full px-2 py-1 border-2 border-pinkPokerBorder shadow-4xl">
-            <Image src={less} alt="lessIcon" />
+          <button className="ml-4 mt-2 mb-2  flex  items-center justify-center bg-pinkPoker rounded-full w-6 h-6 border-2 border-pinkPokerBorder shadow-4xl">
+            <Image src={less} alt="lessIcon" className="" />
           </button>
           {/*Button move*/}
-          <div className=" flex justify-center w-64 border-1 rounded-full m-auto h-3   bg-primary">
-            <Image src={bars} alt="barsIcon" />
+          <div className=" flex justify-center w-64 border-1 rounded-full items-center h-3   bg-primary">
+            <Image src={bars} alt="barsIcon" className="bg-pinkPoker " />
           </div>
           {/*Buttons add*/}
-          <button className="m-3 flex items-center  bg-pinkPoker rounded-full px-2 py-1 border-2 border-pinkPokerBorder shadow-4xl">
+          <button className="mr-4 mt-2 mb-2  flex items-center justify-center w-6 h-6  bg-pinkPoker rounded-full  border-2 border-pinkPokerBorder shadow-4xl">
             <Image src={more} alt="moreIcon" />
           </button>
         </div>
